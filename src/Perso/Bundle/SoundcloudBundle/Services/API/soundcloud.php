1dda4190554241c2f6493c2e134c7c3c0c7452b8
zeypo <zeypodesign@gmail.com>
2014-07-09 11:15:35 +0200

    commit kernel pour heroku

--- src/Perso/Bundle/SoundcloudBundle/Services/API/soundcloud.php
+++ src/Perso/Bundle/SoundcloudBundle/Services/API/soundcloud.php
@@ -54,40 +54,6 @@ class soundcloud
     }
 
     /**
-     * Télécharge les likes soundcloud d'un utilisateur
-     * @param user_id
-     */
-    public function downloadLikes($user_id=null)
-    {
-        $this->user_id = $user_id == null ? $this->user_id : $user_id;
-        $likes = $this->getLikes();
-
-        $zip_path  = __DIR__.'/../../../../../../web/zip/myzip.zip';
-
-        foreach ($likes as $key => $value) {
-            $local_path = str_replace(" ", "_", $likes[$key]['title'].".mp3");
-            $local_path = str_replace("/", "_", $likes[$key]['title'].".mp3");
-            $path = $likes[$key]['link'];
-            
-            $this->save_localy($path, $local_path);
-        }
-
-        $zipfile = $this->create_zip($this->fileToZip, $zip_path);
-
-        // Dl le fichier
-        header('Content-Disposition: attachment; filename=' . basename($zip_path));
-        readfile($zip_path);
-
-        // Supprime les fichiers locaux
-        foreach ($this->fileToZip as $file) {
-            unlink($file);
-        }
-        unlink($zip_path);
-
-        exit;
-    }
-
-    /**
      * Récupère tous les likes d'un user
      * @return likes
      */
@@ -129,73 +95,5 @@ class soundcloud
         } while ($i >= 90);
 
         return $likes;
-    }
-
-    /**
-     * Download song from url
-     * @param url
-     * @param save_path
-     */
-    function save_localy($url, $save_path)
-    {
-        set_include_path('__DIR__."/../../../../../../web/musiques/');
-        // Enregistre le fichier en local
-        // ! Supprimer les fichiers en local lorsque dl terminer
-        $f = fopen( $save_path , 'w+', true);
-        $handle = fopen($url , "rb");
-         
-        while (!feof($handle)) {
-            $contents = fread($handle, 8192);
-            fwrite($f , $contents);
-        }
-
-        chmod($save_path, '0755');
-        fclose($handle);
-        array_push($this->fileToZip, $save_path);
-
-        return;
-    }
-
-    /**
-     * Crée un fichier zip à partir d'un dossier
-     */
-    public function create_zip($files = array(),$destination = '',$overwrite = false)
-    {
-        //if the zip file already exists and overwrite is false, return false
-        if(file_exists($destination) && !$overwrite) { return false; }
-        //vars
-        $valid_files = array();
-        //if files were passed in...
-        if(is_array($files)) {
-            //cycle through each file
-            foreach($files as $file) {
-                //make sure the file exists
-                if(file_exists($file)) {
-                    $valid_files[] = $file;
-                }
-            }
-        }
-        //if we have good files...
-        if(count($valid_files)) {
-            //create the archive
-            $zip = new \ZipArchive();
-            if($zip->open($destination,$overwrite ? \ZIPARCHIVE::OVERWRITE : \ZIPARCHIVE::CREATE) !== true) {
-                return false;
-            }
-            //add the files
-            foreach($valid_files as $file) {
-                $zip->addFile($file,$file);
-            }
-            //debug
-            //echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
-            
-            //close the zip -- done!
-            $zip->close();
-            
-            //check to make sure the file exists
-            return file_exists($destination);
-        } else {
-            return false;
-        }
     }
 }
